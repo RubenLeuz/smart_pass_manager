bbeeba3e784f8178c043314d15b82ff4e9adfa43
Ruben Leuzinger <ruleu@gmx.ch>
2025-06-15 20:32:15 +0200

    __init__.py

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@
 from .smart_transpile import smart_transpile
 from .passes import SmartLayoutPass
 
-__all__ = ["smart_transpile", "SmartLayoutPass"]
+__all__ = ["smart_transpile", "SmartLayoutPass", "__version__"]
+__version__ = "0.1.0"
